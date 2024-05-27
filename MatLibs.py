429616ff13c5cd6bf6c13b10c4bc8d140a8adebb
Pavel Padilla <ppadilla@peruslab.com>
2024-05-27 17:19:10 -0500

    duplique comentario

--- MatLibs.py
+++ MatLibs.py
@@ -8,7 +8,7 @@ Verbo2=input("ingrese otro verbo ")
 lugar= input("ingrese un Lugar (nombre) ")
 
 # OPERACION CON LAS VARIABLES
-
+# OPERACION CON LAS VARIABLES
 matlib= f"Existía hace muchos años atras un {adjetivo} {sustantivo} cuyo nombre era conocido por todo el pueblo, ese maldito nombre era {nombre}.Cuenta la leyenda que una noche decidió {Verbo1} y {Verbo2} a todas sus victimas, enterrandoles por un sombrío sitio llamado {lugar}" 
 print(matlib)
 
