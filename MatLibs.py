f1e766ff098c04d2e01a427764971859b580e14e
Pavel Padilla <ppadilla@peruslab.com>
2024-05-27 17:16:42 -0500

    mi primer commit

---
+++ MatLibs.py
@@ -0,0 +1,14 @@
+#DEFINICION DE VARIABLES
+
+adjetivo= input("ingrese un adjetivo ")
+sustantivo= input("ingrese un Sustantivo: ")
+nombre= input("ingrese un Nombre: ")
+Verbo1= input("ingrese un verbo ")
+Verbo2=input("ingrese otro verbo ")
+lugar= input("ingrese un Lugar (nombre) ")
+
+# OPERACION CON LAS VARIABLES
+
+matlib= f"Existía hace muchos años atras un {adjetivo} {sustantivo} cuyo nombre era conocido por todo el pueblo, ese maldito nombre era {nombre}.Cuenta la leyenda que una noche decidió {Verbo1} y {Verbo2} a todas sus victimas, enterrandoles por un sombrío sitio llamado {lugar}" 
+print(matlib)
+
